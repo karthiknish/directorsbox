62bcb0212b44113a1958f37abaca5f3e3c267c66
karthik <karthik.nishanth06@gmail.com>
2024-12-18 10:46:32 +0000

    add

--- src/components/header.js
+++ src/components/header.js
@@ -5,13 +5,13 @@ import Image from "next/image";
 const Header = () => {
   return (
     <motion.header
-      className="w-full py-12 z-10 relative bg-white border-b border-gray-200"
+      className="w-full lg:py-12 py-4 z-10 relative bg-white border-b border-gray-200"
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6 }}
     >
       <div className="container mx-auto px-4 flex justify-between items-center">
-        <div className="w-40" /> {/* Spacer */}
+        <div className="w-40" />
         <motion.div whileHover={{ scale: 1.05 }} transition={{ duration: 0.2 }}>
           <Link
             href="https://directorsbox.co.uk"
